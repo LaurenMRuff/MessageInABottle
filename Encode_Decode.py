90d266df6d10f7bc658b959003a1115e7b3abe1e
Lauren Ruff <48524322+LaurenMRuff@users.noreply.github.com>
2022-02-14 18:50:57 -0800

    added code for encoding and decoding a message - NOT TESTED

--- Encode_Decode.py
+++ Encode_Decode.py
@@ -6,9 +6,22 @@
 # File: Encode_Decode.py
 # Description: This file will contain the algorithm for encoding or decoding a message from a photo
 
-def encode():
-    pass
 
-def decode():
-    pass
+def encode(img, msg):
+    # open the image for appending, write the message to the image, close the image, and return the image
+    e_img = open(img, mode='a')
+    e_img.write(msg)
+    e_img.close()
+
+    return e_img
+
+
+def decode(img):
+    # open the image for reading, read the message from the image, close the image, and return the message
+    d_img = open(img, mode='r')
+    d_msg = d_img.read()
+
+    d_img.close()
+
+    return d_msg
 
